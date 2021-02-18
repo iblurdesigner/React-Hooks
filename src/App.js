ac0e509618fd85e969c1161291e18f178c0c052f
iblurdesigner <nevomen@gmail.com>
2021-02-18 18:16:06 -0500

    Creado el useContext para agregar a favoritos

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <h1>Rick and Morty</h1>
       <Characters />
     </div>
   );
--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,9 +1,26 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useReducer, useMemo} from 'react'
 import '../styles/characters.css'
+
+const initialState = {
+    favorites: []
+}
+
+const favoriteReducer = (state, action) => {
+    switch (action.type) {
+        case 'ADD_TO_FAVORITE':
+            return {
+                ...state,
+                favorites: [...state.favorites, action.payload]
+            }
+        default:
+            return state;
+    }
+}
 
 const Characters = () => {
 
-    const [characters, setCharacters] = useState([])
+    const [characters, setCharacters] = useState([]);
+    const [favorites, dispatch] = useReducer(favoriteReducer, initialState);
 
     useEffect( () => {
         fetch('https://rickandmortyapi.com/api/character')
@@ -11,13 +28,29 @@ const Characters = () => {
         .then(data => setCharacters(data.results))
     }, [])
 
+    const handleClick = favorite => {
+        dispatch({ type: 'ADD_TO_FAVORITE', payload: favorite })
+    }
+
     return (
         <div className="Characters">
+
+            {favorites.favorites.map(favorite => {
+                return (
+                    <li key={favorite.id}>
+                        {favorite.name}
+                    </li>
+                )
+            })}
+
             {characters.map( character => {
                 return(
-                    <div className="card">
-                        <img src={character.image} alt={character.name}/>
-                        <h2>{character.name}</h2>
+                    <div className="item" key={character.id}>
+                        <div className="card" >
+                            <img src={character.image} alt={character.name}/>
+                            <h3>{character.name}</h3>
+                            <button type="button" onClick={() => handleClick(character)}>Agregar a Favorito</button>
+                        </div>
                     </div>
                 )
             })}
